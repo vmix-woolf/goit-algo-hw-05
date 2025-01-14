b649f83feb512ec3a705fc3be68c102091652b3c
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-01-14 18:37:36 +0200

    add binary search for fractional numbers

---
+++ task02/binary_search_fractional_numbers.py
@@ -0,0 +1,33 @@
+def binary_search_fractional_numbers(array, value):
+    """
+    Implements binary search for the sorted array with fractional numbers
+    :param array: sorted array with fractional numbers
+    :param value: target value for searching
+    :return: tulip (iterations, upper_bound)
+    """
+    left, right = 0, len(arr) - 1
+    iterations = 0
+    upper_bound = None
+
+    while left <= right:
+        iterations += 1
+        mid = (left + right) // 2
+
+        if array[mid] == value:
+            return iterations, array[mid]  # exact match
+
+        if array[mid] < value:
+            left = mid + 1
+        else:
+            upper_bound = array[mid]  # update upper bound
+            right = mid - 1
+
+    # if target value is not found, return upper bound
+    return iterations, upper_bound
+
+
+if __name__ == '__main__':
+    arr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    target = 0.15
+    result = binary_search_fractional_numbers(arr, target)
+    print(f"Total iterations: {result[0]}, Upper bound: {result[1]}")
